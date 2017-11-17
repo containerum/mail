6f03184b06949c6d56e773b47d0b44221551265b
Vladimir Stolyarov <xakep6666@gmail.com>
2017-11-17 17:49:51 +0300

    Add methods for message_storage

--- storages/message_storage.go
+++ storages/message_storage.go
@@ -5,6 +5,9 @@ import (
 
 	"time"
 
+	"encoding/json"
+	"errors"
+
 	"github.com/boltdb/bolt"
 	"github.com/sirupsen/logrus"
 )
@@ -14,6 +17,8 @@ type MessagesStorage struct {
 	log *logrus.Logger
 }
 
+const messagesStorageBucket = "messages"
+
 type MessagesStorageValue struct {
 	UserId       string            `json:"user_id"`
 	TemplateName string            `json:"template_name"`
@@ -21,6 +26,8 @@ type MessagesStorageValue struct {
 	CreatedAt    time.Time         `json:"created_at"` // UTC
 	Message      string            `json:"message"`    // base64
 }
+
+var ErrMessageNotFound = errors.New("message not found")
 
 func NewMessageStorage(file string, options *bolt.Options) (*MessagesStorage, error) {
 	log := logrus.WithField("component", "messages_storage").Logger
@@ -30,8 +37,66 @@ func NewMessageStorage(file string, options *bolt.Options) (*MessagesStorage, er
 		log.WithError(err).Error("Failed to open storage")
 		return nil, err
 	}
+
+	log.Infof("Creating bucket %s", messagesStorageBucket)
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(messagesStorageBucket))
+		return err
+	})
+	if err != nil {
+		log.WithError(err).Error("Create bucket failed")
+		return nil, err
+	}
+
 	return &MessagesStorage{
 		db:  db,
 		log: log,
 	}, nil
+}
+
+// PutValue puts MessageStorageValue to storage.
+// If message with specified id already exists in storage it will be overwritten.
+func (s *MessagesStorage) PutValue(id string, value *MessagesStorageValue) error {
+	loge := s.log.WithFields(logrus.Fields{
+		"id":    id,
+		"value": value,
+	})
+	loge.Infof("Putting value")
+	return s.db.Update(func(tx *bolt.Tx) error {
+		loge.Debug("Get bucket")
+		b := tx.Bucket([]byte(messagesStorageBucket))
+
+		loge.Debug("Marshal json")
+		valueB, err := json.Marshal(value)
+		if err != nil {
+			loge.WithError(err).Error("Error marshalling value")
+		}
+		return b.Put([]byte(id), valueB)
+	})
+}
+
+// GetValue returns value by specified ID.
+func (s *MessagesStorage) GetValue(id string) (*MessagesStorageValue, error) {
+	loge := s.log.WithField("id", id)
+	loge.Infof("Getting value")
+	var value MessagesStorageValue
+	err := s.db.View(func(tx *bolt.Tx) error {
+		loge.Debug("Get bucket")
+		b := tx.Bucket([]byte(messagesStorageBucket))
+
+		loge.Debug("Extract value from storage")
+		valueB := b.Get([]byte(id))
+		if valueB == nil {
+			loge.Info("Cannot find value")
+			return ErrMessageNotFound
+		}
+
+		if err := json.Unmarshal(valueB, &value); err != nil {
+			loge.WithError(err).Error("Value unmarshal failed")
+			return err
+		}
+
+		return nil
+	})
+	return &value, err
 }
