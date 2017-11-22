d68f09e5e1a7e11a46ffea8e0914d2c9b24578f1
Vladimir Stolyarov <xakep6666@gmail.com>
2017-11-22 18:48:12 +0300

    Add main

---
+++ config.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+
+	"bitbucket.org/exonch/ch-mail-templater/storages"
+	"bitbucket.org/exonch/ch-mail-templater/upstreams"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gopkg.in/mailgun/mailgun-go.v1"
+)
+
+func setupLogger() error {
+	switch gin.Mode() {
+	case gin.TestMode, gin.DebugMode:
+	case gin.ReleaseMode:
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+	viper.SetDefault("log_level", logrus.InfoLevel)
+	level := logrus.Level(viper.GetInt("log_level"))
+	if level > logrus.DebugLevel || level < logrus.PanicLevel {
+		return errors.New("invalid log level")
+	}
+	return nil
+}
+
+func getTemplatesStorage() (*storages.TemplateStorage, error) {
+	viper.SetDefault("template_db", "templates.db")
+	file := viper.GetString("template_db")
+	return storages.NewTemplateStorage(file, nil)
+}
+
+func getMessagesStorage() (*storages.MessagesStorage, error) {
+	viper.SetDefault("messages_db", "messages.db")
+	file := viper.GetString("messages_db")
+	return storages.NewMessagesStorage(file, nil)
+}
+
+func getUpstream() (upstreams.Upstream, error) {
+	viper.SetDefault("upstream", "mailgun")
+	switch viper.GetString("upstream") {
+	case "mailgun":
+		mg, err := mailgun.NewMailgunFromEnv()
+		if err != nil {
+			return nil, err
+		}
+		return upstreams.NewMailgun(mg), nil
+	default:
+		return nil, errors.New("invalid upstream")
+	}
+}
+
+func getListenAddr() string {
+	viper.SetDefault("listen_addr", ":7070")
+	return viper.GetString("listen_addr")
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"bitbucket.org/exonch/ch-mail-templater/routes"
+	"github.com/gin-gonic/contrib/ginrus"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func exitOnErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	viper.SetEnvPrefix("ch_mail")
+	viper.AutomaticEnv()
+	exitOnErr(setupLogger())
+
+	app := gin.New()
+	app.Use(gin.Recovery())
+	app.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true))
+
+	ts, err := getTemplatesStorage()
+	exitOnErr(err)
+	ms, err := getMessagesStorage()
+	exitOnErr(err)
+	us, err := getUpstream()
+	exitOnErr(err)
+
+	routes.Setup(app, &routes.Services{
+		TemplateStorage: ts,
+		MessagesStorage: ms,
+		Upstream:        us,
+	})
+	exitOnErr(app.Run(getListenAddr()))
+}
