f45ce0899eda54319d072f1e9046c8c86e505eed
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-08 10:58:26 +0300

    Use utils.Error

--- routes/helpers.go
+++ routes/helpers.go
@@ -4,23 +4,20 @@ import (
 	"net/http"
 
 	"git.containerum.net/ch/mail-templater/storages"
+	"git.containerum.net/ch/utils"
 	"github.com/gin-gonic/gin"
 )
-
-type errorToClient struct {
-	Error string `json:"error"`
-}
 
 func sendStorageError(ctx *gin.Context, err error) {
 	switch err {
 	case nil:
 	case storages.ErrTemplateNotExists, storages.ErrVersionNotExists, storages.ErrMessageNotExists:
-		ctx.AbortWithStatusJSON(http.StatusNotFound, errorToClient{Error: err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusNotFound, utils.Error{Text: err.Error()})
 	default:
 		ctx.AbortWithStatus(http.StatusInternalServerError)
 	}
 }
 
 func sendValidationError(ctx *gin.Context, err error) {
-	ctx.AbortWithStatusJSON(http.StatusBadRequest, errorToClient{Error: err.Error()})
+	ctx.AbortWithStatusJSON(http.StatusBadRequest, utils.Error{Text: err.Error()})
 }
