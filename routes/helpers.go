fdd6779d67bef62fcda7e95287cd439dc1ee1be7
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-21 17:53:52 +0300

    use chutils.NewError to create error

--- routes/helpers.go
+++ routes/helpers.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"git.containerum.net/ch/mail-templater/storages"
-	"git.containerum.net/ch/utils"
+	chutils "git.containerum.net/ch/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,12 +12,12 @@ func sendStorageError(ctx *gin.Context, err error) {
 	switch err {
 	case nil:
 	case storages.ErrTemplateNotExists, storages.ErrVersionNotExists, storages.ErrMessageNotExists:
-		ctx.AbortWithStatusJSON(http.StatusNotFound, utils.Error{Text: err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusNotFound, chutils.NewError(err.Error()))
 	default:
 		ctx.AbortWithStatus(http.StatusInternalServerError)
 	}
 }
 
 func sendValidationError(ctx *gin.Context, err error) {
-	ctx.AbortWithStatusJSON(http.StatusBadRequest, utils.Error{Text: err.Error()})
+	ctx.AbortWithStatusJSON(http.StatusBadRequest, chutils.NewError(err.Error()))
 }
